7e0cc7d67126c6578345df45edecbc496bc85483
minleg <minleg@minleg.com>
2023-02-27 21:07:26 -0600

    cleaned up

--- javascript/index.js
+++
@@ -1,9 +0,0 @@
-let searchQueryInput = document.querySelector('#search-query')
-let submitButton = document.querySelector('#submit-button')
-
-submitButton.addEventListener('click', function() {
-
-    let searchQuery = searchQueryInput.value 
-    console.log(searchQuery)
-
-})
