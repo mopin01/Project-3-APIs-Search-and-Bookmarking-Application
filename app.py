f8c783807144571cbec3de3a5358cd4731b996ab
minleg <minleg@minleg.com>
2023-03-01 01:54:36 -0600

    more efficient code and added comments

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ def homepage():
 @app.route('/get_movie')
 def movie_info():
     movie_title = request.args.get('movie_name')
-    overview, release_date, id, original_title, title = movie_db_api.get_overview(movie_title)
-    image_list = movie_db_api.get_image(id)
-    genre_list, budget, revenue, status, production_companies_list = movie_db_api.more_info(id)
-    return render_template('movie.html', overview=overview, release_date=release_date, id=id, original_title=original_title, title=title, image_list=image_list, genre_list=genre_list, budget=budget, revenue=revenue, status=status, production_companies_list=production_companies_list)
+    overview_data = movie_db_api.get_overview(movie_title)
+    image_list = movie_db_api.get_image(overview_data['id'])
+    genre_list, business_data, production_companies_list = movie_db_api.more_info(overview_data['id'])
+    return render_template('movie.html', overview_data=overview_data, image_list=image_list, genre_list=genre_list, business_data=business_data, production_companies_list=production_companies_list)
      
 
 
