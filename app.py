fb86c59191efd5bc72135baca272f35e15f3c87c
Owner <abdc>
2023-03-14 11:41:11 -0500

    youtube_api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request  # NOT the same as requests
 from apis import movie_db_api 
 from apis import moviequotes_api
+from apis import youtube_trailer_api
 
 app = Flask(__name__)
 
@@ -11,10 +12,11 @@ def homepage():
 @app.route('/get_movie')
 def movie_info():
     movie_title = request.args.get('movie_name')
+    youtube_trailer = youtube_trailer_api.get_movie_trailer(movie_title)
     overview_data = movie_db_api.get_overview(movie_title)
     image_list = movie_db_api.get_image(overview_data['id'])
     genre_list, business_data, production_companies_list = movie_db_api.more_info(overview_data['id'])
-    return render_template('movie.html', overview_data=overview_data, image_list=image_list, genre_list=genre_list, business_data=business_data, production_companies_list=production_companies_list)
+    return render_template('movie.html', overview_data=overview_data, image_list=image_list, genre_list=genre_list, business_data=business_data, production_companies_list=production_companies_list, youtube_trailer=youtube_trailer)
 
 @app.route('/post_quote/')
 def post_quote():
