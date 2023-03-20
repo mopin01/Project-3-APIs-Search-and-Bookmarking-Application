ce23f264551a10fc885b9f8306d5c1264236b371
MostlyRice <amazinzygy@gmail.com>
2023-03-20 15:30:05 -0500

    added comments

--- apis/imbd_api.py
+++ apis/imbd_api.py
@@ -2,22 +2,28 @@ import requests
 import os
 from dotenv import load_dotenv
 
+# Load the environment variables from .env file
 load_dotenv()
 
-apiKey  = os.getenv('IMBD_API_KEY')
+# Get the API key from the environment variables
+apiKey = os.getenv('IMBD_API_KEY')
+
+# Define the API endpoints
 search_url = "https://imdb-api.com/en/API/SearchMovie"
 wikipedia_url = 'https://imdb-api.com/en/API/Wikipedia/'
 
-""" This function gets the movieID of what we're searching """ 
+""" This function gets the movieID of what we're searching """
 def get_imbd_data(name):
     try:
-        # set the headers with API key and content type
+        # Set the headers with API key and content type
         params = {
             'apiKey': apiKey,
             'expression': name
         }
+        # Send a GET request to the IMBD API to search for movies
         response = requests.get(search_url, params=params).json()
 
+        # Extract the ID of the first movie that matches the search
         imbd_data = {
             'id': response.get('results')[0].get('id')
         }
@@ -26,17 +32,18 @@ def get_imbd_data(name):
     except Exception as e:
         print('Unable to get imbd data', e)
 
-""" This function uses the returned movieID and gets the plot summary of that movie """ 
+""" This function uses the returned movieID and gets the plot summary of that movie """
 def get_wikipedia_data(id):
     try:
-        # set the headers with API key and content type
+        # Set the headers with API key and content type
         params = {
             'apiKey': apiKey,
             'id': id
         }
-
+        # Send a GET request to the IMBD API to get the plot summary of a movie
         response = requests.get(wikipedia_url, params=params).json()
 
+        # Extract the plot summary of the movie
         wikiedia_summary = {
             "plotShort": response.get('plotShort').get('plainText')
         }
