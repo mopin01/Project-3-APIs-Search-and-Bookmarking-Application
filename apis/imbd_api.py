0f5ca9a3ae5d066f98def98ba3e796f8f651d3b3
MostlyRice <amazinzygy@gmail.com>
2023-03-27 16:16:15 -0500

    added exception and error handling for imbd_api

--- apis/imbd_api.py
+++ apis/imbd_api.py
@@ -32,8 +32,18 @@ async def get_imbd_data(name):
         }
         return imbd_data
 
+    except (IndexError, KeyError) as e:
+        print('Error getting IMBD data: ', e)
+        raise ValueError('Could not find IMBD data for given movie title.')
+            
+    except aiohttp.ClientError as e:
+        print('Error connecting to IMBD API: ', e)
+        raise ValueError('Unable to connect to IMBD API.') 
+            
     except Exception as e:
-        print('Unable to get imbd data', e)
+        print('Unknown error occurred while getting IMBD data: ', e)
+        raise ValueError('An unknown error occurred while getting IMBD data.')
+
 
 async def get_wikipedia_data(id):
     try:
@@ -52,8 +62,16 @@ async def get_wikipedia_data(id):
         }
         return wikiedia_summary
 
+    except (aiohttp.ClientError, asyncio.TimeoutError) as e:
+        print(f'Error occurred while fetching Wikipedia data: {str(e)}')
+        raise Exception('Error occurred while fetching Wikipedia data.')
+    except KeyError as e:
+        print(f'Error occurred while parsing Wikipedia response: {str(e)}')
+        raise Exception('Error occurred while parsing Wikipedia response.')
     except Exception as e:
-        print('Unable to get imbd data', e)
+        print(f'Unknown error occurred while fetching Wikipedia data: {str(e)}')
+        raise Exception('Unknown error occurred while fetching Wikipedia data.')
+
 
 async def main():
     name = "The Godfather"
